1a75c19af39db25003c573db360b7e8b6d5cc78d
lw23305 <76029562+lw23305@users.noreply.github.com>
2023-11-07 18:54:13 +0800

    Update diy-part2-x86-lean.sh

--- diy-part2-x86-lean.sh
+++ diy-part2-x86-lean.sh
@@ -9,6 +9,10 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.3.2/g' package/base-files/files/bin/config_generate
+sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 
 # 切换ramips内核为 5.10
 sed -i 's/5.15/5.10/g' ./target/linux/ramips/Makefile
