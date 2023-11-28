198de28b129bf719c2a9d4e012139ac0919d0f7b
lw23305 <76029562+lw23305@users.noreply.github.com>
2023-11-28 21:21:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,6 +18,11 @@
 git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
 svn export https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall package/luci-app-passwall
+# 调整 samba4 到 服务 菜单
+sed -i 's/nas/services/g; s/nas/Services/g' feeds/luci/applications/luci-app-samba4/luasrc/controller/samba4.lua
+sed -i 's/nas/services/g' feeds/luci/applications/luci-app-samba4/luasrc/view/samba4/samba4_status.htm
+# 取消对 samba4 的菜单调整
+sed -i '/samba4/s/^/#/' package/lean/default-settings/files/zzz-default-settings
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
