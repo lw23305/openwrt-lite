10b299acddc62009814777eecf035ce4595035e7
lw23305 <76029562+lw23305@users.noreply.github.com>
2024-02-29 22:39:07 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,16 +13,5 @@
 # package/emortal/default-settings/files/99-default-settings
 
 # Modify default IP
-#sed -i 's/192.168.1.1/10.0.0.2/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.3.2/g' package/base-files/files/bin/config_generate
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
-sed -i '/firewall.user/d' package/lean/default-settings/files/zzz-default-settings
-sed -i '/REVISION/d' package/lean/default-settings/files/zzz-default-settings
-sed -i '/DESCRIPTION/d' package/lean/default-settings/files/zzz-default-settings
-sed -i '/filter_aaaa/d' package/network/services/dnsmasq/files/dhcp.conf
-sed -i '/cachesize/d' package/network/services/dnsmasq/files/dhcp.conf
-sed -i '/min_cache_ttl/d' package/network/services/dnsmasq/files/dhcp.conf
-sed -i '/start/d' package/network/services/dnsmasq/files/dhcp.conf
-sed -i '/limit/d' package/network/services/dnsmasq/files/dhcp.conf
-sed -i '/config dnsmasq/r files/dnsmasq.conf' package/network/services/dnsmasq/files/dhcp.conf
-sed -i '/config dhcp lan/r files/dhcp.conf' package/network/services/dnsmasq/files/dhcp.conf
