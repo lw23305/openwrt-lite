68cc093e2639dfdcd9a9e46c28caa26703353270
lw23305 <76029562+lw23305@users.noreply.github.com>
2024-03-07 23:36:15 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,11 +21,7 @@ sed -i "s/KERNEL_TESTING_PATCHVER:=*.*/KERNEL_TESTING_PATCHVER:=5.15/g" target/l
 # git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
 # git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
 # git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
-git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
-git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/passwall
-# Add luci-app-passwall
-# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages
-# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
+
 
 # 替换curl版本回到8.5.0 - 修复 passwall 百度连通性测试失败
 # rm -rf feeds/packages/net/curl
