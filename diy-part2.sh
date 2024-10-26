b530802ae6b93dc89aee5b6f99677bcae8a34fbb
lw23305 <76029562+lw23305@users.noreply.github.com>
2024-10-26 16:24:54 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,5 +11,5 @@
 #
 
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/luci2/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings 
