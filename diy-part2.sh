36786abb3ccabd4d7ec4d1b672dffc41417788da
lw23305 <76029562+lw23305@users.noreply.github.com>
2024-10-30 15:54:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,5 +11,4 @@
 #
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-
 # sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings 
