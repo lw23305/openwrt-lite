e9253e7a0618ca2ccbb8b949faf071e6b7b3d784
lw23305 <76029562+lw23305@users.noreply.github.com>
2025-03-07 09:03:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -39,8 +39,8 @@ function git_sparse_clone() {
 }
 
 # ssr
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
+# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 git_clone https://github.com/immortalwrt/homeproxy luci-app-homeproxy
 git_clone https://github.com/sbwml/packages_lang_golang golang
 
